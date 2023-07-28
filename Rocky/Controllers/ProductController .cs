954f5a5e44f4008e0d29697d01a9ef678f8b2055
abeyshekeeva <abeyshekeeva@PC-202Z-170523W>
2023-07-28 10:55:11 +0600

    57

--- Rocky/Controllers/ProductController .cs
+++ Rocky/Controllers/ProductController .cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 
 namespace Rocky.Controllers
@@ -11,9 +15,11 @@ namespace Rocky.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public ProductController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -67,15 +73,34 @@ namespace Rocky.Controllers
         //POST - Upsert
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Upsert(Product obj)
+        public IActionResult Upsert(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                _db.Product.Add(obj);
-                _db.SaveChanges();
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                if (productVM.Product.Id == 0)
+                {
+                    //creating
+                    string upload = webRootPath + WC.ImagePath;
+                    string fileName=Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+                    productVM.Product.Image = fileName + extension;
+                    _db.Product.Add(productVM.Product); 
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    //update
+                }
                 return RedirectToAction("Index");
             }
-            return View(obj);
+            return View();
         }
 
         //GET - Delete

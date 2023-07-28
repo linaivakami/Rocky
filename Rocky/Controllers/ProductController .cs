02e0ccda7058e233e3f291747ae857de598b520a
abeyshekeeva <abeyshekeeva@PC-202Z-170523W>
2023-07-28 11:53:04 +0600

    59

--- Rocky/Controllers/ProductController .cs
+++ Rocky/Controllers/ProductController .cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
@@ -83,7 +84,7 @@ namespace Rocky.Controllers
                 {
                     //creating
                     string upload = webRootPath + WC.ImagePath;
-                    string fileName=Guid.NewGuid().ToString();
+                    string fileName = Guid.NewGuid().ToString();
                     string extension = Path.GetExtension(files[0].FileName);
 
                     using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
@@ -91,13 +92,40 @@ namespace Rocky.Controllers
                         files[0].CopyTo(fileStream);
                     }
                     productVM.Product.Image = fileName + extension;
-                    _db.Product.Add(productVM.Product); 
-                    _db.SaveChanges();
+                    _db.Product.Add(productVM.Product);
+
                 }
                 else
                 {
-                    //update
+                    //updating
+                    var objfromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagePath;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var oldFile = Path.Combine(upload, objfromDb.Image);
+
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productVM.Product.Image = fileName + extension;
+                    }
+                    else
+                    {
+                        productVM.Product.Image = objfromDb.Image;
+                    }
+                    _db.Product.Update(productVM.Product);
                 }
+                _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View();
@@ -110,24 +138,32 @@ namespace Rocky.Controllers
             {
                 return NotFound();
             }
-            var obj = _db.Category.Find(id);
-            if (obj == null)
+            Product product = _db.Product.Include(u => u.Category).FirstOrDefault(u => u.Id == id);
+            if (product == null)
             {
                 return NotFound();
             }
-            return View(obj);
+            return View(product);
         }
         //POST - Delete
-        [HttpPost]
+        [HttpPost,ActionName("Delete")]
         [AutoValidateAntiforgeryToken]
         public IActionResult DeletePost(int? id)
         {
-            var obj = _db.Category.Find(id);
+            var obj = _db.Product.Find(id);
             if (obj == null)
             {
                 return NotFound();
             }
-            _db.Category.Remove(obj);
+            string upload = _webHostEnvironment.WebRootPath + WC.ImagePath;
+
+            var oldFile = Path.Combine(upload, obj.Image);
+
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+            _db.Product.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
 

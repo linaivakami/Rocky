083219dc8f422340ed8d7e29710d786f7b26837a
abeyshekeeva <abeyshekeeva@PC-202Z-170523W>
2023-07-31 11:07:19 +0600

    63

--- Rocky/Models/Product.cs
+++ Rocky/Models/Product.cs
@@ -10,6 +10,7 @@ namespace Rocky.Models
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ShortDesc { get; set; }
         [Required]
         [Range(1,int.MaxValue)]
         public double Price { get; set; }
